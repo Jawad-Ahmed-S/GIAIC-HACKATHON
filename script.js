6260267aa9118d2c33cede8643afb0676cf7398f
Jawad Ahmed <soomrojawad1.2.2006@gmail.com>
2024-09-17 15:27:00 +0500

    --

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-// Get references to the elements
+
 const btnjs = document.getElementById('btnjs');
 const btnH = document.querySelector('.btnH');
 
-// Add event listener to the button
 btnjs.addEventListener('click', () => {
   btnH.classList.toggle('btnH-h');
 });
